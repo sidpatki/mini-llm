68348c6cb6118eba942345ec9dfce46233cb0a8d
A Meta HPC user for spatki <spatki@submit-6.fair-a100.hpcaas>
2024-08-14 20:17:24 +0000

    Add config support

--- demo_mini_llm.py
+++ demo_mini_llm.py
@@ -2,69 +2,80 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from mini_llm_model import MiniLlmModel
+import argparse
+import json
 
-# Hyperparameters Small Scale
-context_length = 8
-batch_size = 32
-n_heads = 4
-n_layers = 4
-eval_interval = 1000
-eval_iterations = 1000
-max_iters = 10000
-emb_size = 32
-learning_rate = 1e-3
-dropout = 0.2
+def load_config(path):
+    """
+    Method to load config file
+    """
+    with open(path, 'r') as file:
+        config = json.load(file)
+    return config
 
-# Hyperparameters Large Scale
-# batch_size = 64
-# context_length = 1024
-# n_heads = 4
-# n_layers = 4
-# eval_interval = 1000
-# eval_iterations = 1000
-# max_iters = 5000
-# emb_size = 256
-# learning_rate = 3e-4
-# dropout = 0.2
+def parse_args():
+    """
+    Method to parse input arguments
+    """
 
-# Declare seed for reproducibility
-torch.manual_seed(1337)
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # Create an ArgumentParser object
+    parser = argparse.ArgumentParser(description='Demo to run the mini-llm model')
 
-# Open the file
-with open('input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+    # Define expected arguments
+    parser.add_argument('--corpus', type=str, required=True, help='The input .txt file for generating vocab')
+    parser.add_argument('--model', type=str, required=True, help='The model .pth file to run')
+    parser.add_argument('--config', type=str, required=True, help='The config .json file')
 
-# Make the token vocabulary (all chars and punctuations)
-vocab = sorted(list(set(text)))
-vocab_size = len(vocab)
-print(vocab)
-print(vocab_size)
+    # Parse the arguments
+    args = parser.parse_args()
 
-# Create char to int and reverse mapping
-# Make tokenizer
-stoi = { char : i for i, char in enumerate(vocab)}
-itos = { i : char for i, char in enumerate(vocab)}
+    return args
 
-tokenize = lambda s: [ stoi[char] for char in s]
-detokenize = lambda l: "".join([ itos[i] for i in l])
+if __name__ == "__main__":
 
-# Create model
-model = MiniLlmModel(vocab_size, emb_size, context_length, n_heads, n_layers)
-model.load_state_dict(torch.load('char_llm.pth'))
-model = model.to(device)
-total_params = sum(p.numel() for p in model.parameters())
-print(f"This model has {total_params} parameters")
-model.eval()
+    # Get the input arguments
+    args = parse_args()
 
-# Try generating sample text
-idx = torch.zeros((1,1), dtype=torch.long, device=device)
-print(detokenize(model.generate(idx, max_new_tokens=400)[0].tolist()))
+    # Get the config parameters
+    config = load_config(args.config)
 
-# Try generating text with custom prompt
-prompt = "Hello world, this is a large language model. My name is?"
-idx = torch.tensor(tokenize(prompt), dtype=torch.long, device=device)
-idx = idx.reshape(1, len(prompt))
-print("\n\n\n")
-print(detokenize(model.generate(idx, max_new_tokens=400)[0].tolist()))
-breakpoint()
+    # Declare seed for reproducibility
+    torch.manual_seed(1337)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # Open the file
+    with open(args.corpus, 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    # Make the token vocabulary (all chars and punctuations)
+    vocab = sorted(list(set(text)))
+    vocab_size = len(vocab)
+    print(vocab)
+    print(vocab_size)
+
+    # Create char to int and reverse mapping
+    # Make tokenizer
+    stoi = { char : i for i, char in enumerate(vocab)}
+    itos = { i : char for i, char in enumerate(vocab)}
+
+    tokenize = lambda s: [ stoi[char] for char in s]
+    detokenize = lambda l: "".join([ itos[i] for i in l])
+
+    # Create model
+    model = MiniLlmModel(vocab_size, config.get("emb_size"), config.get("context_length"), config.get("n_heads"), config.get("n_layers"))
+    model.load_state_dict(torch.load(args.model))
+    model = model.to(device)
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"This model has {total_params} parameters")
+    model.eval()
+
+    # Try generating sample text
+    idx = torch.zeros((1,1), dtype=torch.long, device=device)
+    print(detokenize(model.generate(idx, max_new_tokens=400)[0].tolist()))
+
+    # Try generating text with custom prompt
+    prompt = "Hello world, this is a large language model. My name is?"
+    idx = torch.tensor(tokenize(prompt), dtype=torch.long, device=device)
+    idx = idx.reshape(1, len(prompt))
+    print("\n\n\n")
+    print(detokenize(model.generate(idx, max_new_tokens=400)[0].tolist()))
--- train_mini_llm.py
+++ train_mini_llm.py
@@ -2,138 +2,160 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from mini_llm_model import MiniLlmModel
+import argparse
+import json
 
-# Declare seed for reproducibility
-torch.manual_seed(1337)
+def load_config(path):
+    """
+    Method to load config file
+    """
+    with open(path, 'r') as file:
+        config = json.load(file)
+    return config
 
-# # Hyperparameters Small Scale
-# context_length = 8
-# batch_size = 32
-# n_heads = 4
-# n_layers = 4
-# eval_interval = 1000
-# eval_iterations = 1000
-# max_iters = 10000
-# emb_size = 32
-# learning_rate = 1e-3
-# dropout = 0.2
+def parse_args():
+    """
+    Method to parse input arguments
+    """
 
-# Hyperparameters Large Scale
-batch_size = 64
-context_length = 256
-n_heads = 4
-n_layers = 4
-eval_interval = 1000
-eval_iterations = 1000
-max_iters = 5000
-emb_size = 256
-learning_rate = 3e-4
-dropout = 0.2
+    # Create an ArgumentParser object
+    parser = argparse.ArgumentParser(description='Demo to run the mini-llm model')
 
-# Fetch device
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # Define expected arguments
+    parser.add_argument('--corpus', type=str, required=True, help='The input .txt file for generating vocab')
+    parser.add_argument('--config', type=str, required=True, help='The config .json file')
 
-# Open the file
-with open('input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+    # Parse the arguments
+    args = parser.parse_args()
 
-# Print first 1000 chars of the file
-# print(text[0:1000])
+    return args
 
-# Make the token vocabulary (all chars and punctuations)
-vocab = sorted(list(set(text)))
-vocab_size = len(vocab)
-print(vocab)
-print(vocab_size)
+if __name__ == "__main__":
 
-# Create char to int and reverse mapping
-# Make tokenizer
-stoi = { char : i for i, char in enumerate(vocab)}
-itos = { i : char for i, char in enumerate(vocab)}
+    # Get the input arguments
+    args = parse_args()
 
-tokenize = lambda s: [ stoi[char] for char in s]
-detokenize = lambda l: "".join([ itos[i] for i in l])
+    # Get the config parameters
+    config = load_config(args.config)
 
-print(detokenize(tokenize("happy to build char-llm")))
+    # Declare seed for reproducibility
+    torch.manual_seed(1337)
 
-# Tokenize the entire data and store it in a tensor
-data = torch.tensor(tokenize(text))
+    # Hyperparameters Large Scale
+    batch_size = config.get("batch_size")
+    context_length = config.get("context_length")
+    n_heads = config.get("n_heads")
+    n_layers = config.get("n_layers")
+    eval_interval = config.get("eval_interval")
+    eval_iterations = config.get("eval_iterations")
+    max_iters = config.get("max_iters")
+    emb_size = config.get("emb_size")
+    learning_rate = config.get("learning_rate")
 
-# Split data into train and val
-n = int(len(data) * 0.9)
-train_data = data[0:n]
-val_data = data[n:]
+    # Fetch device
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# Example showing fetching one block of training
-# samples and respective targets
-x = train_data[:context_length]
-y = train_data[1:context_length+1]
-# for i in range(context_length):
-#     print(f"when training sample is {x[:i+1]} target is {y[i]}")
+    # Open the file
+    with open(args.corpus , 'r', encoding='utf-8') as f:
+        text = f.read()
 
+    # Print first 1000 chars of the file
+    # print(text[0:1000])
 
-def get_batch(split, batch_size=4):
-    '''
-    Method to get minibatch of data
-    '''
-    data = train_data if split == "train" else val_data
-    indices = torch.randint(len(data) - context_length, (batch_size,))
-    x = torch.stack( [data[i:i+context_length] for i in indices] )
-    y = torch.stack( [data[i+1:i+context_length+1] for i in indices] )
-    x = x.to(device)
-    y = y.to(device)
-    return x, y 
+    # Make the token vocabulary (all chars and punctuations)
+    vocab = sorted(list(set(text)))
+    vocab_size = len(vocab)
+    print(vocab)
+    print(vocab_size)
 
-# Create model
-model = MiniLlmModel(vocab_size, emb_size, context_length, n_heads, n_layers)
-model = model.to(device)
-total_params = sum(p.numel() for p in model.parameters())
-print(f"This model has {total_params} parameters")
+    # Create char to int and reverse mapping
+    # Make tokenizer
+    stoi = { char : i for i, char in enumerate(vocab)}
+    itos = { i : char for i, char in enumerate(vocab)}
 
-# Test the forward pass
-xb, yb = get_batch('train')
-logits, loss = model(xb, yb)
+    tokenize = lambda s: [ stoi[char] for char in s]
+    detokenize = lambda l: "".join([ itos[i] for i in l])
 
-# Make optimizer object
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    print(detokenize(tokenize("happy to build char-llm")))
 
-# Method to calculate average loss on train and val batches
-@torch.no_grad()
-def estimate_loss(eval_iterations):
-    out = {}
-    model.eval()
-    for split in ['train', 'val']:
-        losses = torch.zeros(eval_iterations)
-        for k in range(eval_iterations):
-            x, y = get_batch(split)
-            logits, loss = model(x, y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    return out
+    # Tokenize the entire data and store it in a tensor
+    data = torch.tensor(tokenize(text))
+
+    # Split data into train and val
+    n = int(len(data) * 0.9)
+    train_data = data[0:n]
+    val_data = data[n:]
+
+    # Example showing fetching one block of training
+    # samples and respective targets
+    x = train_data[:context_length]
+    y = train_data[1:context_length+1]
+    # for i in range(context_length):
+    #     print(f"when training sample is {x[:i+1]} target is {y[i]}")
 
 
-# Training loop
-model.train()
-for i in range(max_iters):
+    def get_batch(split, batch_size=4):
+        '''
+        Method to get minibatch of data
+        '''
+        data = train_data if split == "train" else val_data
+        indices = torch.randint(len(data) - context_length, (batch_size,))
+        x = torch.stack( [data[i:i+context_length] for i in indices] )
+        y = torch.stack( [data[i+1:i+context_length+1] for i in indices] )
+        x = x.to(device)
+        y = y.to(device)
+        return x, y 
 
-    # Every once in a while calculate loss on train and val sets
-    if i % eval_interval == 0 or i == (max_iters-1):
-        losses = estimate_loss(eval_iterations=eval_iterations)
-        print(f"iteration {i}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+    # Create model
+    model = MiniLlmModel(vocab_size, emb_size, context_length, n_heads, n_layers)
+    model = model.to(device)
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"This model has {total_params} parameters")
 
-    xb, yb = get_batch('train', batch_size=batch_size)
-
+    # Test the forward pass
+    xb, yb = get_batch('train')
     logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
 
-model.eval()
+    # Make optimizer object
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
-# Save the trained model
-torch.save(model.state_dict(), 'char_llm.pth')
-print("Model saved to char_llm.pth")
+    # Method to calculate average loss on train and val batches
+    @torch.no_grad()
+    def estimate_loss(eval_iterations):
+        out = {}
+        model.eval()
+        for split in ['train', 'val']:
+            losses = torch.zeros(eval_iterations)
+            for k in range(eval_iterations):
+                x, y = get_batch(split)
+                logits, loss = model(x, y)
+                losses[k] = loss.item()
+            out[split] = losses.mean()
+        return out
 
-# Try generating sample text
-idx = torch.zeros((1,1), dtype=torch.long, device=device)
-print(detokenize(model.generate(idx, max_new_tokens=400)[0].tolist()))
+
+    # Training loop
+    model.train()
+    for i in range(max_iters):
+
+        # Every once in a while calculate loss on train and val sets
+        if i % eval_interval == 0 or i == (max_iters-1):
+            losses = estimate_loss(eval_iterations=eval_iterations)
+            print(f"iteration {i}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+        xb, yb = get_batch('train', batch_size=batch_size)
+
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+
+    model.eval()
+
+    # Save the trained model
+    torch.save(model.state_dict(), 'char_llm.pth')
+    print("Model saved to char_llm.pth")
+
+    # Try generating sample text
+    idx = torch.zeros((1,1), dtype=torch.long, device=device)
+    print(detokenize(model.generate(idx, max_new_tokens=400)[0].tolist()))
